0907eea4780b29e9d738794e8afba664d1fc17f1
Arun Ramesh <arun.ramesh@redblacksoftware.com>
2019-04-15 21:29:34 +0530

    initial push

---
+++ lambda/lambda_function.py
@@ -0,0 +1,32 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    
+    operation = event['http_method']
+
+    if operation == 'GET':
+        lst = []
+        log = boto3.client('logs', event['region'])
+        response = log.get_query_results(
+            queryId=event['queryid']
+        )
+        return response
+        
+    elif operation == 'POST':
+        log = boto3.client('logs', event['body']['region'])
+        response = log.start_query(
+			logGroupName=event['body']['logGroupName'],
+			startTime=int(event['body']['startTime']),
+			endTime=int(event['body']['endTime']),
+			queryString=event['body']['queryString']
+		)
+		
+        return response
+		#{'queryId':response['queryId']}
+    #{
+    #    'statusCode': 200,
+    #    'body': json.dumps('Hello from Lambda!')
+    #}
+#3b14dd58-ac96-4c4e-b13c-f1f0d989d3b0
